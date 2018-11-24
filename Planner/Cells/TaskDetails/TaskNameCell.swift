ba9735128f83e9daa5264460f4d2aa63473dcedb
Konstantin <grahovsky@gmail.com>
2018-11-24 17:44:47 +0300

    #UITextFieldDelegate #delegate #resignFirstResponder

--- Planner/Cells/TaskDetails/TaskNameCell.swift
+++ Planner/Cells/TaskDetails/TaskNameCell.swift
@@ -8,19 +8,27 @@
 
 import UIKit
 
-class TaskNameCell: UITableViewCell {
+class TaskNameCell: UITableViewCell, UITextFieldDelegate {
 
     @IBOutlet weak var textTaskName: UITextField!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        textTaskName.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    // при нажатии Enter
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textTaskName.resignFirstResponder() // скрыть фокус с текстового поля (клавиатура исчезнет)
+        return true
     }
 
 }
